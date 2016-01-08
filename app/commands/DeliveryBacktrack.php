1c993a2feebc29b8368be6341ea7da05c5684630
Andy Awidarto <andy.awidarto@gmail.com>
2016-01-08 22:55:07 +0700

    backtrack

---
+++ app/commands/DeliveryBacktrack.php
@@ -0,0 +1,75 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class DeliveryBacktrack extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'backtrack:delivery';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Delivery Backtrack';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		$delivereds = Orderlog::where('appname','=',Config::get('jex.tracker_app'))
+                        ->where('status','=','delivered')
+                        ->orderBy('created_at','desc')
+                        ->groupBy('deliveryId')
+                        ->get();
+
+        foreach($delivereds as $d){
+            print $d->deliveryId." ".$d->deliverytime."\r\n";
+        }
+	}
+
+	/**
+	 * Get the console command arguments.
+	 *
+	 * @return array
+	 */
+	protected function getArguments()
+	{
+		return array(
+			array('example', InputArgument::OPTIONAL, 'An example argument.'),
+		);
+	}
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+		);
+	}
+
+}
