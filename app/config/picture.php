471e45aad6c731881330e77d164e2ec107ef1479
Andy Awidarto <andy.awidarto@gmail.com>
2016-01-11 14:36:31 +0700

    picture sizes

--- app/config/picture.php
+++ app/config/picture.php
@@ -2,10 +2,10 @@
 
 return array(
     'sizes'=>array(
-        'thumbnail'=>array('width'=>234, 'height'=>60,'prefix'=>'th_'),
-        'medium'=>array('width'=>300, 'height'=>100,'prefix'=>'med_'),
-        'medium_portrait'=>array('width'=>486, 'height'=>60,'prefix'=>'med_port_'),
-        'large'=>array('width'=>728, 'height'=>90,'prefix'=>'lrg_'),
-        'full'=>array('width'=>720, 'height'=>90,'prefix'=>'full_'),
+        'thumbnail'=>array('width'=>80, 'height'=>60,'prefix'=>'th_'),
+        'medium'=>array('width'=>400, 'height'=>300,'prefix'=>'med_'),
+        'medium_portrait'=>array('width'=>300, 'height'=>400,'prefix'=>'med_port_'),
+        'large'=>array('width'=>640, 'height'=>480,'prefix'=>'lrg_'),
+        'full'=>array('width'=>800, 'height'=>600,'prefix'=>'full_'),
         ),
     );
