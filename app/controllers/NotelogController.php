800a6156dfc04f17d3fc428df562e28e29ea828e
Andy Awidarto <andy.awidarto@gmail.com>
2016-01-20 17:50:50 +0700

    delivery note log

--- app/controllers/NotelogController.php
+++ app/controllers/NotelogController.php
@@ -7,7 +7,8 @@ class NotelogController extends AdminController {
             array('Delivery Id',array('search'=>true,'sort'=>true)),
             array('Status',array('search'=>true,'sort'=>false)),
             array('Delivery Note',array('search'=>true,'sort'=>true)),
-            array('Device Name',array('search'=>true,'sort'=>true))
+            array('Device Name',array('search'=>true,'sort'=>true)),
+            array('App Name',array('search'=>true,'sort'=>true))
 
         );
 
@@ -16,7 +17,8 @@ class NotelogController extends AdminController {
             array('deliveryId',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('status',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
             array('note',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
-            array('deviceId',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true))
+            array('deviceId',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true)),
+            array('appname',array('kind'=>'text','query'=>'like','pos'=>'both','show'=>true))
         );
 
 
