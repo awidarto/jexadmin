9ad9028bc9f5c511bd9242527c30787a29c19d69
Andy Awidarto <andy.awidarto@gmail.com>
2016-03-03 16:57:04 +0700

    loc log ffid

--- public/themes/default/views/locationlog/addfilter.blade.php
+++ public/themes/default/views/locationlog/addfilter.blade.php
@@ -260,7 +260,7 @@
                                 var link ='<a href="#" class="use-loc" id="'+ d.delivery_id +'" data-lat="'+ d.lat +'" data-lon="'+ d.lng +'">use loc</a>';
 
                                 mlink = $(link).on('click',function(e){
-                                            console.log(this);
+                                            console.log( $(this) );
                                             var lat = $(this).data('lat');
                                             var lon = $(this).data('lon');
                                             var delivery_id = $(this).id;
