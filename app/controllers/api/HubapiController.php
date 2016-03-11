9f5abccf503cbc789ed5c39fcc072b7861f9205d
Andy Awidarto <andy.awidarto@gmail.com>
2016-03-11 15:51:37 +0700

    wh dl status

--- app/controllers/api/HubapiController.php
+++ app/controllers/api/HubapiController.php
@@ -44,7 +44,6 @@ class HubapiController extends \BaseController {
             'pickupPerson',
             'dirLat',
             'dirLon',
-            'status',
             'laststatus',
             'pendingCount',
             'changeActor',
